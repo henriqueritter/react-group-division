e7846c5ccb5fcc1f5ee00024835d235ddc757ba6
Henrique Ritter <henrique-ritter@hotmail.com>
2021-12-25 12:32:36 -0300

    created frame structure of Main scene

--- src/scenes/Main/index.tsx
+++ src/scenes/Main/index.tsx
@@ -3,18 +3,21 @@ import * as React from "react";
 export const Main: React.FC = () => {
 
   return (
-    <div>
-      <header>
-        <span>Criar Grupos com itens balanceados</span>
+    <div id="mainContainer">
+      <header id="mainHeader">
+        <span id="mainHeaderSpan">Criar Grupos com itens balanceados</span>
       </header>
       <div>
-        <textarea name="itensTextArea" id="itensTextArea"></textarea>
-        <select name="groupsQuantity" id="groupsQuantity">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-        <button name="submitItensButton" id="submitItensButton" onClick={() => alert('Button')} >Gerar grupos</button>
+        <div id="inputContainer">
+          <textarea name="itensTextArea" id="itensTextArea"></textarea>
+          <select name="groupsQuantity" id="groupsQuantity">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <button name="submitItensButton" id="submitItensButton" onClick={() => alert('Button')} >Gerar grupos</button>
+        </div>
+        <div id="responseContainer"></div>
       </div>
     </div>
   )
